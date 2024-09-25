bf7ff354c752b078cb7ed66503b9d5a50b48b200
SabariGanesh-K <k.sabarii.ganesh@gmail.com>
2024-09-25 23:10:26 +0530

    REFACTORING Skills and constants util changes [DONE]

--- src/app/config/constants.tsx
+++ src/app/config/constants.tsx
@@ -39,10 +39,10 @@ export const careerItems: CareerItems[] = [
     company: "Nethermind,London,United Kingdom",
     description: [
       "Worked with a Internationally distributed team remotely. Involved and Contributed to \n Multiple Projects .",
-      "SEDGE :-a one-click node setup tool for PoS network/chain validators and nodes written entirely in the Go programming language. Sedge takes care of the entire onpremise full node setup based on the chosen client, using generated docker-compose scripts based on the desired configuration. Worked extensively in Go Lang involving Cobra packages configuring the terminal commandlines and controls as per requirements and features for a smooth setup of sedge and nodes.",
+      "SEDGE :- a one-click node setup tool for PoS network/chain validators and nodes written entirely in the Go programming language. Sedge takes care of the entire onpremise full node setup based on the chosen client, using generated docker-compose scripts based on the desired configuration. Worked extensively in Go Lang involving Cobra packages configuring the terminal commandlines and controls as per requirements and features for a smooth setup of sedge and nodes.",
       " ZkSYNC - Remix Plugin :- Worked with the ZkSYNC - Nethermind team on the Remix plugin enabling developers to effortlessly deploy and interact with zkSync contracts. Worked on Vite + Typescript and Rust for API. Built with Rocket , a framework for Rust",
       "PAPRIKA:- custom implementation of the Patricia tree used in Ethereum. It aims at replacing the underlying storage as a solution on a higher level of abstraction. PagedDb component utilizes memory-mapped files for efficient storage, employing Copy on Write for concurrency management, inspired by LMBD. ( Uses c# with .NET primarily). Contributed in the pagedDB part for optimization involving garbage collections etc ",
-      "Voyager:- Was part of the Starknet block explorer Voyager team. Worked with Tech stacks Involving Vite , Typescript with PostgreSQL backends",
+      "Voyager:- Was part of the Starknet block explorer Voyager team. Worked with Tech stacks Involving Vite , Typescript with PostgreSQL backends,Grafana for Monitoring",
     ],
     href: "#",
   },
@@ -52,8 +52,8 @@ export const careerItems: CareerItems[] = [
     company: "Hashstack Finance,Bengaluru,India",
     description: [
       "Contributed to Hashstack's mission of creating trustless financial products for underserved populations worldwide.",
-      "Led software development of Open protocol, enabling secure under-collateralized crypto loans in starknet with high-quality UI and APIs.",
-      "Worked with Next with typescript, Next APIs and modern webdev blockchain tech. Also worked in Devops part involving Github Actions , AWS , etc.",
+      "Led software development of Open protocol, enabling secure under-collateralized asset token loans  in starknet with high-quality UI and APIs.",
+      "Worked with Next with typescript, Next APIs and modern webdev = tech. Also worked in Devops part involving Github Actions , AWS , etc.",
       "Ensuring responsive design for diverse devices also working with offchain involving AWS and blockchain endpoints+Oracles for integration and permission management with deployment",
       "Maintained clean, reusable code following design patterns with comprehensive tests.",
     ],
@@ -64,7 +64,7 @@ export const careerItems: CareerItems[] = [
     duration: "January  2023 - December 2024",
     company: "Golddust Finance",
     description: [
-      "Leveraging DeFi Investments involving Arbitrages , Yield Farming using the power of AI",
+      "Leveraging Digital Assets and Crypto Investments involving Arbitrages , Yield Farming using the power of AI",
       "Used Next with TYPESCRIPT and Solidity with tokenomics and plannings with analysis on trade profits including protocols like Uniswap  ,sushiswap. Algorithmic strategizing of flashswaps between liquidity pools",
       "Worked with founder who have already built a web3 startup with 1 Million funding in the industry",
     ],
@@ -75,8 +75,8 @@ export const careerItems: CareerItems[] = [
     duration: "April  2023 - June 2024",
     company: "EDMYN",
     description: [
-      "Revolutionized the textbook experience by working with leading academic author in the field of Computer Science Mrs.Sumita Arora and CTO Kushagra Arora.",
-      "iWorked on stacks nvolving Google Firebase Firestore and cloud functions",
+      "Revolutionized the textbook experience by working with leading National level academic author in the field of Computer Science Mrs.Sumita Arora and CTO Kushagra Arora.",
+      "Worked on Scalable, real-time database architecture and Firebase backend implementation .Cloud Functions to automate workflows",
       "Early stage startup experience with Founding team",
     ],
     href: "#",
@@ -84,7 +84,7 @@ export const careerItems: CareerItems[] = [
 ];
 
 export const resumeLink: string =
-  "https://drive.google.com/file/d/1qyJ5YV0leOEsMf9BWWSy_hgXzSWobclH/view?usp=sharing";
+  "https://drive.google.com/file/d/1T2B2QMh_2vlJ7V_0ftqApaanFqcRbq1d/view";
 export const beyondresumeLink: string =
   "https://drive.google.com/drive/folders/1aHhXCBLkixtnjM8--B8Kwl7N3npKOMJk?usp=sharing";
 export const skillSets: SkillSet[] = [
@@ -99,7 +99,7 @@ export const skillSets: SkillSet[] = [
   },
   {
     genre: "DevOps and Cloud",
-    stack: "AWS cloud , Docker , Github Actions , Kubernetes , Jenkins,Pipelines,IAAC with Terraform , Redis  ",
+    stack: "AWS cloud , Docker , Github Actions , Kubernetes , Jenkins,Pipelines, Redis  ",
   },
   {
     genre: "Artificial Intelligence",
@@ -121,7 +121,7 @@ export const skillSets: SkillSet[] = [
   },
   {
     genre: "Hardware:",
-    stack: "Arduino UNO ,IOT,C, Cyber physical systemS ,ESP 32 , Raspberry pi",
+    stack: "Arduino UNO , IOT and domain analysis, C , Cyber physical systems  ,ESP 32 , Raspberry pi , Autonomous Drones , Embedded systems ",
   },
 ];
 
